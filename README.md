704d6371873d1bdf6ecd24d39241d562e6c10d0b
LohitaPanapana <lohita59@yahoo.com>
2019-06-28 21:41:55 +0530

    UI changes

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Weather-App
-Weather App with Node.js
+
+This web app is developed using Node.js as primary language and uses [Open weather](https://openweathermap.org/api) API to get weather data. Visit weather app at [https://weathercloudapp.herokuapp.com/](https://weathercloudapp.herokuapp.com/)
+![Weather App](weatherapp.png)
+Using this app, current weather can be found out based on 
+1. city name
+2. city id
+3. zipcode
+
+In addition to displaying current weather, this app all display the weather forcast for next **5 days**.
+## Getting Started
+
+###Prereqisites
+
+Get [Open weather website](https://openweathermap.org/api) and get an API KEY to query the API for weather data.
+
+This project makes two API calls 
+1. [Current Weather](api.openweathermap.org/data/2.5/weather?q=London) - To fetch current weather
+2. [5 days/ 3 hours](api.openweathermap.org/data/2.5/forecast?q=London,us&mode=xml) - To fetch weather forcast for next 5 days.
+
+
--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const request = require("request");
 const app = express();
@@ -7,23 +8,19 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.locals.moment = require('moment');
 
-
-// let url = `http://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=d0783d7b08a751209bc77567140ebbb5`;
-
 //Get weather data by making a call to weather API
 app.get("/", function(req, res){
     let location = req.query.search || 'London';
-    let today = new Date();
-    let date = today.getFullYear() + '-' + (today.getMonth()+1) + '-' +today.getDate();
-    let url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=d0783d7b08a751209bc77567140ebbb5`;
+
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${process.env.API_KEY}`;
     request(url, function(error, response, body){
         if(!error && response.statusCode == 200){
             let currentData = JSON.parse(body);
-            let forcastUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${location}&units=metric&appid=d0783d7b08a751209bc77567140ebbb5`;
+            let forcastUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${location}&units=metric&appid=${process.env.API_KEY}`;
             request(forcastUrl, function(error, response, body){
                 let forcastData = JSON.parse(body);
                 let weatherMap = code.getUnique(forcastData.list);
-                res.render("weather", {weather: currentData, date: date, forcastData: weatherMap});
+                res.render("weather", {weather: currentData, forcastData: weatherMap});
             })
         }
     });
